0b974aa4e4883b7c377b56ff6b674bf3d997088b
Valentin Picard <valentin.picard@zenika.com>
2024-08-08 13:53:09 -0400

    Correction TP7

---
+++ Exercises/correction/lab7/src/pages/ShowListItemDetails.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container" v-if="show">
+    <h1 class="title">{{ show.title }}</h1>
+    <CardShow :show="show" @toggle-favorite="store.toggleFavorite(show.id)" />
+  </div>
+</template>
+
+<script setup>
+import CardShow from '@/components/CardShow.vue'
+import { computed } from 'vue'
+import { useStore } from '@/store'
+
+const props = defineProps(['showId'])
+
+const store = useStore()
+const show = computed(() => store.shows.find((show) => show.id == props.showId))
+</script>
